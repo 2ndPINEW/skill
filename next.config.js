3da7aca61542a006094a9bdcb3c1ce9b0bfe2f94
Eito Takase <eito01178@gmail.com>
2023-02-03 19:30:20 +0900

    apikeyを環境変数に追加

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config()
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    MICRO_CMS_SERVICE_NAME: process.env.SERVICE,
+    MICRO_CMS_API_KEY: process.env.APIKEY
+  }
 }
 
 module.exports = nextConfig
