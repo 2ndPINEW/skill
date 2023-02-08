195ec4c9694fc9d136838e1f84968bedd986cf3f
Eito Takase <eito01178@gmail.com>
2023-02-08 19:26:40 +0900

    バッジのレイアウトを整える

--- src/components/SkillBadge.tsx
+++ src/components/SkillBadge.tsx
@@ -1,13 +1,25 @@
-import Link, { LinkProps } from "next/link"
-import { ReactNode } from "react"
+import { skills } from "@/types/cms-types";
+import Image from "next/image";
+import Link, { LinkProps } from "next/link";
+import styles from './SkillBadge.module.scss'
 
 type SkillBadgeProp = {
-  id: string
-  children: ReactNode
-} & Omit<LinkProps, 'href'>
+  content: skills<"get">;
+} & Omit<LinkProps, "href">;
 
-export const SkillBadge = ({ id, children, ...props }: SkillBadgeProp) => {
+export const SkillBadge = ({ content, ...props }: SkillBadgeProp) => {
   return (
-    <Link href={`/skills/${id}`} {...props}>{children}</Link>
-  )
-}
+    <Link href={`/skills/${content.id}`} {...props}>
+      {content.logo_image ? (
+        <Image
+          src={content.logo_image.url}
+          alt={content.label}
+          width="30"
+          height="30"
+        ></Image>
+      ) : (
+        <div className={styles.text_label}>{content.label}</div>
+      )}
+    </Link>
+  );
+};
