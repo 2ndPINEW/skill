66a71fe700f08a2d9a07fe4439f190d1ea286798
Eito Takase <eito01178@gmail.com>
2023-02-08 19:30:59 +0900

    レイアウトを適当に整えた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from './page.module.scss'
 import { SkillBadge } from '@/components/SkillBadge'
 import { endPoints } from '@/pages/api/api'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default async function Home() {
   const data = await endPoints.gets('skills')
@@ -12,40 +8,18 @@ export default async function Home() {
 
   return (
     <main className={styles.main}>
-      {
-        contents?.map((content) => {
-          return <SkillBadge key={content.id} id={content.id}>{content.label}</SkillBadge>
-        })
-      }
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+      <div className={styles.axis_area}>
+        <div className={`${styles.label} ${styles.label_love}`}>🥰</div>
+        <div className={`${styles.label} ${styles.label_dislike}`}>😱</div>
+        <div className={`${styles.label} ${styles.label_good_at}`}>😎</div>
+        <div className={`${styles.label} ${styles.label_bad_at}`}>😓</div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className={styles.skill_area}>
+        {
+          contents?.map((content) => {
+            return <SkillBadge key={content.id} content={content} />
+          })
+        }
       </div>
     </main>
   )
