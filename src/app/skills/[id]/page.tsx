1234de54cf29e45f18c146dc66a235eff5da6428
Eito Takase <eito01178@gmail.com>
2023-02-09 19:23:48 +0900

    チョットできる表示

--- src/app/skills/[id]/page.tsx
+++ src/app/skills/[id]/page.tsx
@@ -1,12 +1,27 @@
+import { endPoints } from '@/pages/api/api'
 import Link from 'next/link'
 
 interface Params {
   id: string
 }
 
-export default function Skill ({ params }: { params: Params }) {
+export default async function Skill ({ params }: { params: Params }) {
+  // get('skills', params.id)でできると思ったら出てこんかった
+  const data = await endPoints.gets('skills', {
+    ids: params.id
+  })
+  const content = data?.contents?.[0]
+
+  function test(): string {
+    if (!content) return ''
+    if (content.forte_rate > 0.9) return 'チョットできる'
+    if (content.forte_rate > 0.5) return 'なんもわからん'
+    if (content.forte_rate > 0.1) return '完全に理解した'
+    return '全くわからん'
+  }
+
   return <>
-    <Link href="/">top</Link>
-    <div>{params.id}</div>
+    <div>{content?.label}{test()}</div>
+    <Link href="/">スキルマップに戻る</Link>
   </>
 }
